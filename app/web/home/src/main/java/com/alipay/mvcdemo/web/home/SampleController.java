2682e60a6a3bf93cb2eb20d2d4da31d8e11f901d
沈东佳 <dongjia.shen@alibaba-inc.com>
2017-08-10 15:39:21 +0800

    通过代码获取配置--用 SpringBean 去实现 AppConfigurationAware 接口，sofa会自动将AppConfigurationAware  这个对象注入到对应的 SpringBean  中，这样就可以通过AppConfigurationAware  里面的API获取到 sofa-config.properies里面的配置

--- app/web/home/src/main/java/com/alipay/mvcdemo/web/home/SampleController.java
+++ app/web/home/src/main/java/com/alipay/mvcdemo/web/home/SampleController.java
@@ -7,11 +7,39 @@ package com.alipay.mvcdemo.web.home;
 
 //import com.alipay.mvcdemo.SampleService;
 import com.alipay.mvcdemo.web.home.SpringBean;
+import com.alipay.sofa.runtime.api.aware.AppConfigurationAware;
+import com.alipay.sofa.runtime.api.component.AppConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.ModelMap;
+
+
+/**
+ * A sample controller.
+ * 在spring配置中使用 sofa-config变量来进行变量替换
+ * by dongjia.shen 20170810
+ */
+@Controller
+public class SampleController implements AppConfigurationAware{
+
+    @Autowired
+    private SpringBean springBean;
+    private AppConfiguration appConfiguration;
+
+    @RequestMapping(value="/sample",method = RequestMethod.GET)
+    public void doGet(ModelMap modelMap) {
+
+        modelMap.put("hello",appConfiguration.getPropertyValue("hello_001"));
+
+    }
+
+    @Override
+    public void setAppConfiguration(AppConfiguration appConfiguration) {
+        this.appConfiguration = appConfiguration;
+    }
+}
 
 ///**
 // * A sample controller.
@@ -31,19 +59,24 @@ import org.springframework.ui.ModelMap;
 //}
 
 
-/**
- * A sample controller.
- */
-@Controller
-public class SampleController {
+///**
+// * A sample controller.
+// * 在spring配置中使用 sofa-config变量来进行变量替换
+// * by dongjia.shen 20170810
+// */
+//@Controller
+//public class SampleController {
+//
+//    @Autowired
+//    private SpringBean springBean;
+//
+//    @RequestMapping(value="/sample",method = RequestMethod.GET)
+//    public void doGet(ModelMap modelMap) {
+//
+//        modelMap.put("hello",springBean.getWorld());
+//
+//    }
+//}
 
-    @Autowired
-    private SpringBean springBean;
 
-    @RequestMapping(value="/sample",method = RequestMethod.GET)
-    public void doGet(ModelMap modelMap) {
 
-        modelMap.put("hello",springBean.getWorld());
-
-    }
-}
