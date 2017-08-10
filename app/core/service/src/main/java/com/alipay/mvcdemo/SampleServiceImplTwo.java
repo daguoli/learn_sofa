2e5f743dee6e0d490a8552cd0866268a761f6d0f
沈东佳 <dongjia.shen@alibaba-inc.com>
2017-08-10 17:23:13 +0800

    若是同一个接口有多个实现类，需要用到unique id

---
+++ app/core/service/src/main/java/com/alipay/mvcdemo/SampleServiceImplTwo.java
@@ -0,0 +1,12 @@
+package com.alipay.mvcdemo;
+
+/**
+ * @author dongjia.shen 20170810
+ */
+
+public class SampleServiceImplTwo implements SampleService {
+    @Override
+    public String hello() {
+        return "Hello Unique Id";
+    }
+}
