85dca553eb8a7d9da2a52144920730b957ba7aa8
沈东佳 <dongjia.shen@alibaba-inc.com>
2017-08-10 17:23:33 +0800

    若是同一个接口有多个实现类，需要用到unique id

--- app/web/home/src/main/java/com/alipay/mvcdemo/web/home/SampleController.java
+++ app/web/home/src/main/java/com/alipay/mvcdemo/web/home/SampleController.java
@@ -5,7 +5,7 @@
 package com.alipay.mvcdemo.web.home;
 
 
-//import com.alipay.mvcdemo.SampleService;
+import com.alipay.mvcdemo.SampleService;
 import com.alipay.mvcdemo.web.home.SpringBean;
 import com.alipay.sofa.runtime.api.aware.AppConfigurationAware;
 import com.alipay.sofa.runtime.api.component.AppConfiguration;
@@ -16,47 +16,47 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.ModelMap;
 
 
-/**
- * A sample controller.
- * 在spring配置中使用 sofa-config变量来进行变量替换
- * by dongjia.shen 20170810
- */
-@Controller
-public class SampleController implements AppConfigurationAware{
-
-    @Autowired
-    private SpringBean springBean;
-    private AppConfiguration appConfiguration;
-
-    @RequestMapping(value="/sample",method = RequestMethod.GET)
-    public void doGet(ModelMap modelMap) {
-
-        modelMap.put("hello",appConfiguration.getPropertyValue("hello_001"));
-
-    }
-
-    @Override
-    public void setAppConfiguration(AppConfiguration appConfiguration) {
-        this.appConfiguration = appConfiguration;
-    }
-}
-
 ///**
 // * A sample controller.
+// * 在spring配置中使用 sofa-config变量来进行变量替换
+// * by dongjia.shen 20170810
 // */
 //@Controller
-//public class SampleController {
+//public class SampleController implements AppConfigurationAware{
 //
 //    @Autowired
-//    private SampleService sampleService;
+//    private SpringBean springBean;
+//    private AppConfiguration appConfiguration;
 //
-//	@RequestMapping(value="/sample",method = RequestMethod.GET)
-//	public void doGet(ModelMap modelMap) {
+//    @RequestMapping(value="/sample",method = RequestMethod.GET)
+//    public void doGet(ModelMap modelMap) {
 //
-//       modelMap.put("hello",sampleService.hello());
+//        modelMap.put("hello",appConfiguration.getPropertyValue("hello_001"));
 //
-//	}
+//    }
+//
+//    @Override
+//    public void setAppConfiguration(AppConfiguration appConfiguration) {
+//        this.appConfiguration = appConfiguration;
+//    }
 //}
+
+/**
+ * A sample controller.
+ */
+@Controller
+public class SampleController {
+
+    @Autowired
+    private SampleService sampleService;
+
+	@RequestMapping(value="/sample",method = RequestMethod.GET)
+	public void doGet(ModelMap modelMap) {
+
+       modelMap.put("hello",sampleService.hello());
+
+	}
+}
 
 
 ///**
